83bc7d3459acf0bc91db1f3738d2ad5f2d3a4daa
pulinza11811 <103770001+pulinza11811@users.noreply.github.com>
2024-01-13 10:34:04 +0700

    Update Webtest

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('app');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) =>{
     res.send('Hello worlds');
