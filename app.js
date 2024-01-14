1678c813c5c223267fd67b3c6136a40586e3c245
pulinza11811 <103770001+pulinza11811@users.noreply.github.com>
2024-01-14 08:34:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
-const debug = require('debug')('app');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const debug = require("debug")("app");
+const path = require("path");
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
-app.set("view engine","ejs")
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-app.get("/products")
+productRouter.route("/").get((req, res) => {
+  res.send("Hello World !! I'm Product");
+});
 
-app.get("/", (req,res) =>{
-    res.render('index',{username: 'Owen', customers: ["Kitti","Kittikorn","Kitty"]});
-})
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello World !! I'm Product 1");
+  });
 
-app.listen(PORT, ()=>{
-    debug("Listening on port %d",PORT);
-})
+app.use("/products", productRouter);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "Owen",
+    customers: ["Kitti", "Kittikorn", "Kitty"],
+  });
+});
+
+app.listen(PORT, () => {
+  debug("Listening on port %d", PORT);
+});
