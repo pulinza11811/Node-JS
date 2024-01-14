105d2334f29ec759220c83180e76273020913858
pulinza11811 <103770001+pulinza11811@users.noreply.github.com>
2024-01-14 08:03:15 +0700

    index_old to index.ejs

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const debug = require('debug')('app');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine","ejs")
+
 app.get("/", (req,res) =>{
-    res.send('Hello worlds');
+    res.render('index',{username: 'Owen', customers: ["Kitti","Kittikorn","Kitty"]});
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port %d",port);
+app.listen(PORT, ()=>{
+    debug("Listening on port %d",PORT);
 })
